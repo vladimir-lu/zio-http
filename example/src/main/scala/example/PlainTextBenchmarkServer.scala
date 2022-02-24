b3bc858adbbe2fe838b998c8675568622545f8d3
shrutiverma97 <shruti@dream11.com>
2022-02-24 13:28:43 +0530

    added json response

--- example/src/main/scala/example/PlainTextBenchmarkServer.scala
+++ example/src/main/scala/example/PlainTextBenchmarkServer.scala
@@ -12,6 +12,10 @@ import zio.{App, ExitCode, UIO, URIO}
 object Main extends App {
 
   private val message: String = "Hello, World!"
+  private val json: String    = """{"greetings": "Hello World!"}"""
+
+  val path1 = "/plaintext"
+  val path2 = "/json"
 
   private val STATIC_SERVER_NAME = AsciiString.cached("zio-http")
 
@@ -21,17 +25,26 @@ object Main extends App {
     .withServer(STATIC_SERVER_NAME)
     .freeze
 
-  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
-    frozenResponse
-      .flatMap(server(_).make.useForever)
-      .provideCustomLayer(ServerChannelFactory.auto ++ EventLoopGroup.auto(8))
-      .exitCode
-  }
-  private val path                                               = "/plaintext"
-  private def app(response: Response) = Http.fromHExit(HExit.succeed(response)).whenPath(path)
+  private val frozenJsonResponse = Response
+    .json(json)
+    .withServerTime
+    .withServer(STATIC_SERVER_NAME)
+    .freeze
 
-  private def server(response: Response) =
-    Server.app(app(response)) ++
+  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
+    val s = for {
+      res1 <- frozenResponse
+      res2 <- frozenJsonResponse
+    } yield server(res1, res2)
+    s.flatMap(_.make.useForever.provideCustomLayer(ServerChannelFactory.auto ++ EventLoopGroup.auto(8))).exitCode
+  }
+
+  private def app(response: Response, jsonResponse: Response) =
+    Http.fromHExit(HExit.succeed(response)).whenPath(path1) ++
+      Http.fromHExit(HExit.succeed(jsonResponse)).whenPath(path2)
+
+  private def server(response: Response, jsonResponse: Response) =
+    Server.app(app(response, jsonResponse)) ++
       Server.port(8080) ++
       Server.error(_ => UIO.unit) ++
       Server.disableLeakDetection ++
