34beb45ecbeec05f9b3daa8bc0c71ff07a587395
shrutiverma97 <shruti@dream11.com>
2022-03-02 17:55:46 +0530

    check

---
+++ project/JmhBenchmarkFlow.scala
@@ -0,0 +1,41 @@
+import BuildHelper.{JmhVersion, Scala213}
+import sbtghactions.GenerativePlugin.autoImport.{UseRef, WorkflowJob, WorkflowStep}
+
+
+object JmhBenchmarkWorkflow {
+
+
+  val jmhPlugin = s"""addSbtPlugin("pl.project13.scala" % "sbt-jmh" % "${JmhVersion}")"""
+
+
+  def apply(): Seq[WorkflowJob] = Seq(
+    WorkflowJob(
+      runsOnExtraLabels = List("zio-http"),
+      id = "runJmhBenchMarks",
+      name = "Jmh Benchmarks",
+      oses = List("centos"),
+      scalas = List(Scala213),
+      steps = List(
+        WorkflowStep.Use(
+          UseRef.Public("actions", "setup-java", s"v2"),
+          Map(
+            "distribution" -> "temurin",
+            "java-version" -> "8"
+          ),
+        ),
+        WorkflowStep.Run(
+          commands = List("cd zio-http", s"sed -i -e '$$a${jmhPlugin}' project/plugins.sbt"),
+          id = Some("add_plugin"),
+          name = Some("Add jmh plugin"),
+        ),
+        WorkflowStep.Run(
+          commands = List("cd zio-http", s"sbt zhttpBenchmarks/jmh:run -i 3 -wi 3 -f1 -t1 CookieDecodeBenchmark"),
+          id = Some("jmh HttpCollectEval"),
+          name = Some("jmh HttpCollectEval"),
+        ),
+      ),
+    ),
+  )
+
+
+}
