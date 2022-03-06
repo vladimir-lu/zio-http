e2c722b70a7f24c7ffffc577b56e1e8ef9cbc8ec
shrutiverma97 <shruti@dream11.com>
2022-03-06 19:08:48 +0530

    wip

--- project/JmhBenchmarkWorkFlow.scala
+++ project/JmhBenchmarkWorkFlow.scala
@@ -14,19 +14,20 @@ object JmhBenchmarkWorkFlow {
   def lists = files.zipWithIndex.map((file: ((Path, FileAttributes), Int)) => {
     val path = file._1._1.toString
     val str = path.replaceAll("^.*[\\/\\\\]", "").replaceAll(".scala", "")
-    (List(s"""sbt -v "zhttpBenchmarks/jmh:run -i 3 -wi 3 -f1 -t1 $str" | tee result${file._2}""",
-      s"""RESULT_REQUEST${file._2}=$$(echo $$(grep "thrpt" result${file._2}))""",
-      s"""echo "$$RESULT_REQUEST${file._2}"""",
-      s"""echo ::set-output name=benchmark_result${file._2}::$$(echo "$$RESULT_REQUEST${file._2}")"""),str)
+    (List(s"""sbt -v "zhttpBenchmarks/jmh:run -i 3 -wi 3 -f1 -t1 $str" | tee result_${file._2}""",
+      s"""RESULT_REQUEST_${file._2}=$$(echo $$(grep "thrpt" result_${file._2}))""",
+      s"""echo "$$RESULT_REQUEST_${file._2}"""",
+      s"""echo ::set-output name=benchmark_result_${file._2}::$$(echo "$$RESULT_REQUEST_${file._2}")"""),str)
   })
 
 //   def jmh(b: Int) = lists.grouped(b).toList
 
+  val result: String = lists.map(x => s"""$${{steps.result.outputs.benchmark_result_${x._2}}}""").mkString("\n|")
 
-  def jmhBenchmark() = lists.map((x: (List[String], String)) =>
+  def jmhBenchmark() = Seq(
     WorkflowJob(
       runsOnExtraLabels = List("zio-http"),
-      id = s"run_Jmh_BenchMarks_${x._2}",
+      id = s"run_Jmh_BenchMarks",
       name = "Jmh_Benchmarks",
       oses = List("centos"),
       scalas = List(Scala213),
@@ -38,12 +39,14 @@ object JmhBenchmarkWorkFlow {
             "java-version" -> "8"
           ),
         ),
+      ) ++  lists.map(x =>
         WorkflowStep.Run(
           env = Map("GITHUB_TOKEN" -> "${{secrets.ACTIONS_PAT}}"),
           commands = List("cd zio-http", s"sed -i -e '$$a${jmhPlugin}' project/plugins.sbt") ++ x._1,
-          id = Some("result"),
-          name = Some("result"),
-        ),
+          id = Some(s"result_${x._2}"),
+          name = Some(s"result_${x._2}"),
+        )
+      ) ++ List(
         WorkflowStep.Use(
           ref = UseRef.Public("peter-evans", "commit-comment", "v1"),
           params = Map(
@@ -52,7 +55,7 @@ object JmhBenchmarkWorkFlow {
               s"""
                 |**\uD83D\uDE80 Jmh Benchmark:**
                 |
-                |$${{steps.result.outputs.benchmark_result_${x._2}}}""".stripMargin,
+                |${result}""".stripMargin,
           ),
         ),
       )
